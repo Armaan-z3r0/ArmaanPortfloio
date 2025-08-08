25f050f10df4f212cd219e19ceb7640999ba20dc
Armaan Mulla <armaan.mulla001@gmail.com>
2025-08-08 12:23:17 +0530

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import linux from "../public/linux.jpg" ;
+import cybersec from "../public/sybersec.jpg"
+import reactimg from "../public/Reactimg.png"
+import nextimg from "../public/Nextimg.png" 
+import djangoimg from "../public/Djangoimg.png"
 function App() {
   return (
     <>
       <div style={{ display : "flex" , justifyContent : "space-between"}}>
         <h1 style={{ fontSize: "100px"  }}>Please Hire Me</h1>
-        <img src="../public/linux.jpg" />
-        <img src="../public/sybersec.jpg" style={{ width : "300px" , height : "300px"}}/>
+        <img src={linux} />
+        <img src={cybersec} style={{ width : "300px" , height : "300px"}}/>
       </div>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' , }}>
-        <img src="../public/Reactimg.png" />
-        <img src="../public/Nextimg.png" />
-        <img src="../public/Djangoimg.png" />
+        <img src={reactimg} />
+        <img src={nextimg} />
+        <img src={djangoimg} />
       </div>
     </>
   )
