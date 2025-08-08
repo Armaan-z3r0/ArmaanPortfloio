50ea544245ebf28762667e9c1ead0c316707e39c
Armaan Mulla <armaan.mulla001@gmail.com>
2025-08-08 12:28:36 +0530

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import linux from "../public/linux.jpg" ;
-import cybersec from "../public/sybersec.jpg"
-import reactimg from "../public/Reactimg.png"
-import nextimg from "../public/Nextimg.png" 
-import djangoimg from "../public/Djangoimg.png"
+import linux from "/linux.jpg" ;
+import cybersec from "/sybersec.jpg"
+import reactimg from "/Reactimg.png"
+import nextimg from "/Nextimg.png" 
+import djangoimg from "/Djangoimg.png"
 function App() {
   return (
     <>
       <div style={{ display : "flex" , justifyContent : "space-between"}}>
         <h1 style={{ fontSize: "100px"  }}>Please Hire Me</h1>
-        <img src={linux} />
-        <img src={cybersec} style={{ width : "300px" , height : "300px"}}/>
+        <img src={linux} alt="linux-img"/>
+        <img src={cybersec} alt="cybersec-img" style={{ width : "300px" , height : "300px"}}/>
       </div>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' , }}>
-        <img src={reactimg} />
-        <img src={nextimg} />
-        <img src={djangoimg} />
+        <img src={reactimg} alt="react-img" />
+        <img src={nextimg} alt="next-img"/>
+        <img src={djangoimg} alt="django-img"/>
       </div>
     </>
   )
