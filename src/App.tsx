7ff4f82a0ad9a8b3ae3aac8f164e12894b8700e5
Armaan Mulla <armaan.mulla001@gmail.com>
2025-08-08 11:32:55 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div style={{ display : "flex" , justifyContent : "space-between"}}>
+        <h1 style={{ fontSize: "100px"  }}>Please Hire Me</h1>
+        <img src="../src/assests/linux.jpg" />
+        <img src="../src/assests/sybersec.jpg" style={{ width : "300px" , height : "300px"}}/>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' , }}>
+        <img src="../src/assests/Reactimg.png" />
+        <img src="../src/assests/Nextimg.png" />
+        <img src="../src/assests/Djangoimg.png" />
+      </div>
+    </>
+  )
+}
+
+export default App
