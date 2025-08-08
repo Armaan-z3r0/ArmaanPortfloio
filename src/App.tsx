80b07e7406aa357411b31b1d8e42710bccdc8966
Armaan Mulla <armaan.mulla001@gmail.com>
2025-08-08 12:13:00 +0530

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ function App() {
     <>
       <div style={{ display : "flex" , justifyContent : "space-between"}}>
         <h1 style={{ fontSize: "100px"  }}>Please Hire Me</h1>
-        <img src="../src/assests/linux.jpg" />
-        <img src="../src/assests/sybersec.jpg" style={{ width : "300px" , height : "300px"}}/>
+        <img src="../public/linux.jpg" />
+        <img src="../public/sybersec.jpg" style={{ width : "300px" , height : "300px"}}/>
       </div>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' , }}>
-        <img src="../src/assests/Reactimg.png" />
-        <img src="../src/assests/Nextimg.png" />
-        <img src="../src/assests/Djangoimg.png" />
+        <img src="../public/Reactimg.png" />
+        <img src="../public/Nextimg.png" />
+        <img src="../public/Djangoimg.png" />
       </div>
     </>
   )
